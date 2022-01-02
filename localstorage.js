e094e4af9959730327b76378cd3d8fcac216b396
olsiq <64708189+olsiq@users.noreply.github.com>
2022-01-02 17:56:42 +0200

    update

--- localstorage.js
+++ localstorage.js
@@ -3,8 +3,53 @@
 //examble 1:3 ==> 63
 //variables
 //const scoresBtn = document.getElementById("scores");
+<<<<<<< Updated upstream
 
 //check the level of the game
+=======
+//
+const returnNullScores = (level) => {
+  const scoresTable = [
+    { name: "-", time: "-" },
+    { name: "-", time: "-" },
+    { name: "-", time: "-" },
+    { name: "-", time: "-" },
+    { name: "-", time: "-" },
+  ];
+
+  localStorage.setItem(level, JSON.stringify(scoresTable));
+};
+//check if the level of the game has stored any scores.if not insert null scores
+
+const checkLocalStorage = (level) => {
+  console.log(level);
+  switch (level) {
+    case "easy": {
+      localStorage.getItem("easy") !== null
+        ? console.log("there is smth there")
+        : returnNullScores(level);
+      break;
+    }
+    case "difficult": {
+      localStorage.getItem("difficult") !== null
+        ? console.log("there is smth there")
+        : returnNullScores(level);
+      break;
+    }
+    case "expert": {
+      localStorage.getItem("expert") !== null
+        ? console.log("there is smth there")
+        : returnNullScores(level);
+      break;
+    }
+    default: {
+      console.log("not an accepted value");
+      break;
+    }
+  }
+};
+
+>>>>>>> Stashed changes
 let level;
 
 let player1 = { name: "as1", time: "100" };
